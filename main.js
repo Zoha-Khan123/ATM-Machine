00c00c37942aebadc2829c0de1198c2493507e90
urusa24 <168416713+urusa24@users.noreply.github.com>
2024-05-06 06:18:56 -0700

    Update main.js
    correction in shebang file

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-#! usr/bin/env node
+#! /usr/bin/env node
 //                                      "ATM MACHINE"
 //============================================================================================
 import inquirer from "inquirer"; //Import Inquirer 
